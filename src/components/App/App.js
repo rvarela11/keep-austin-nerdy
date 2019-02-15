78a79b7d3274f0c362fb4d58f131edcc9650e9c5
Robert Varela <rvarela@utexas.edu>
2019-02-15 09:56:22 -0600

    Added Header and images folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 // @vendors
 import React from 'react';
 
+// @components
+import Header from '../Header/Header';
+
 // @styles
 import './App.scss';
 
 const App = () => (
     <div className="App">
-        Howdy
+        <Header />
     </div>
 );
 
