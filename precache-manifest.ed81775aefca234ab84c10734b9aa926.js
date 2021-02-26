51ef1d84ad4c85933554d7c84a8b21e769c2c3a1
Robert Varela <robertvarela@pingidentity.com>
2021-02-26 14:17:35 -0600

    Updates

--- precache-manifest.ed81775aefca234ab84c10734b9aa926.js
+++ precache-manifest.ed81775aefca234ab84c10734b9aa926.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "294ebaa92292d4c6ffe0",
-    "url": "/keep-austin-nerdy/static/css/main.748b79c2.chunk.css"
+    "revision": "5dd5d75f0b12d4f5d2f2",
+    "url": "/keep-austin-nerdy/static/css/main.dc48df8b.chunk.css"
   },
   {
-    "revision": "294ebaa92292d4c6ffe0",
-    "url": "/keep-austin-nerdy/static/js/main.a1b63cd3.chunk.js"
+    "revision": "5dd5d75f0b12d4f5d2f2",
+    "url": "/keep-austin-nerdy/static/js/main.98e88f68.chunk.js"
   },
   {
     "revision": "e85c8b5a90942bea188e",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/keep-austin-nerdy/static/media/keep_austin_nerdy.e382fb5f.jpg"
   },
   {
-    "revision": "92d01202c72865d30f8d117d0d4624d4",
+    "revision": "243df2e86390159969c2300a89aaf9ca",
     "url": "/keep-austin-nerdy/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/keep-austin-nerdy/precache-manifest.4f13d2fadbadaece25b06ab4c21e2e0b.js"
+  "/keep-austin-nerdy/precache-manifest.ed81775aefca234ab84c10734b9aa926.js"
 );
 
 workbox.clientsClaim();
